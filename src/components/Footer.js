15e93cbd536255866e24d2c5c26d9a3cce47ada5
Sambhav Saxena <76242518+sambhavsaxena@users.noreply.github.com>
2021-12-21 11:15:59 +0530

    Create Footer.js

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <div>
+            <div style={{display:'flex', justifyContent:'center', alignItems:'center', marginTop:'-20px'}}>By&nbsp;<a target="_blank" rel="noreferrer" href="https://github.com/sambhavsaxena">Sambhav Saxena</a></div>
+        </div>
+    )
+}
+
+export default Footer
