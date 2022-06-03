2b0dc7974538151d85ca9432e1b9abd612ff5118
Sambhav Saxena <sambhavsaxena02@gmail.com>
2022-06-03 13:02:12 +0530

    added loader animation on login submit

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -6,32 +6,28 @@ import { Flex, Heading, IconButton, Input } from "@chakra-ui/react"
 import { RiArrowRightLine } from "react-icons/ri"
 import { useToast } from "@chakra-ui/react"
 import { UsersContext } from '../../usersContext'
+import { Spinner } from '@chakra-ui/react'
 import Footer from '../Footer'
 
 const Login = () => {
     const socket = useContext(SocketContext)
     const { name, setName, room, setRoom } = useContext(MainContext)
     const history = useHistory()
+    const [isLoading, setIsLoading] = React.useState(false)
     const toast = useToast()
     const { setUsers } = useContext(UsersContext)
-
-    //Checks to see if there's a user already present
-
     useEffect(() => {
         socket.on("users", users => {
             setUsers(users)
         })
     })
-
     const handleKeyDown = (ev) => {
-        //Send on enter:
         if (ev.keyCode === 13) {
             if ((!!name || !!room) || (name || !!room) || (!!name || room)) handleClick()
         }
     }
-
-    //Emits the login event and if successful redirects to chat
     const handleClick = () => {
+        setIsLoading(true)
         socket.emit('login', { name, room }, error => {
             if (error) {
                 return toast({
@@ -39,17 +35,18 @@ const Login = () => {
                     title: "Error",
                     description: error,
                     status: "error",
-                    duration: 2000,
+                    duration: 3000,
                     isClosable: true,
                 })
             }
             history.push('/chat')
+            setIsLoading(false)
             return toast({
                 position: "top",
                 title: "Hey there",
                 description: `Welcome to ${room}`,
                 status: "success",
-                duration: 3200,
+                duration: 3000,
                 isClosable: true,
             })
         })
@@ -62,8 +59,10 @@ const Login = () => {
             <Flex className="form" gap='1rem' flexDirection={{ base: "column", md: "row" }} style={{ marginTop: '50px' }}>
                 <Input onKeyDown={handleKeyDown} variant='filled' mr={{ base: "0", md: "4" }} mb={{ base: "4", md: "0" }} type="text" placeholder='User Name' autoFocus maxLength={'25'} value={name} onChange={e => setName(e.target.value.toLowerCase().trim().split(/ +/).join(' '))} />
                 <Input onKeyDown={handleKeyDown} variant='filled' mr={{ base: "0", md: "4" }} mb={{ base: "4", md: "0" }} type="text" placeholder='Room Name' value={room} maxLength={'25'} onChange={e => setRoom(e.target.value.toLowerCase().trim().split(/ +/).join(' '))} />
-                <IconButton colorScheme='blue' isRound='true' icon={<RiArrowRightLine />} onClick={handleClick}></IconButton>
+                <IconButton colorScheme='blue' isRound='true' icon={isLoading ? <Spinner size='xl' /> : <RiArrowRightLine />} onClick={handleClick}></IconButton>
             </Flex>
+            <div>
+            </div>
         </Flex>
     )
 }
