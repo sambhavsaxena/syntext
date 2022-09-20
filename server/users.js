be501642be961c505dcd2eaf3f77ae987498e906
Sambhav Saxena <76242518+sambhavsaxena@users.noreply.github.com>
2022-09-20 13:51:51 +0530

    configured server and app in the same repository

---
+++ server/users.js
@@ -0,0 +1,29 @@
+const users = []
+
+const addUser = (id, name, room) => {
+    const existingUser = users.find(user => user.name.trim().toLowerCase() === name.trim().toLowerCase())
+
+    if (existingUser) return { error: "Username has already been taken" }
+    if (!name && !room) return { error: "Username and room are required" }
+    if (name.length < 3) return { error: "Username must be at least three characters" }
+    if (room.length < 3) return { error: "Room name must be at least three characters" }
+    if (!name) return { error: "Username is required" }
+    if (!room) return { error: "Room is required" }
+    const user = { id, name, room }
+    users.push(user)
+    return { user }
+}
+
+const getUser = id => {
+    let user = users.find(user => user.id == id)
+    return user
+}
+
+const deleteUser = (id) => {
+    const index = users.findIndex((user) => user.id === id);
+    if (index !== -1) return users.splice(index, 1)[0];
+}
+
+const getUsers = (room) => users.filter(user => user.room === room)
+
+module.exports = { addUser, getUser, deleteUser, getUsers }
