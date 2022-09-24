5ee64cc42bf31c40746fee171b2d216cf38c2437
Sambhav Saxena <76242518+sambhavsaxena@users.noreply.github.com>
2022-09-24 10:34:57 +0530

    proxified comment

--- src/socketContext.js
+++ src/socketContext.js
@@ -2,7 +2,7 @@ import React, { createContext } from 'react'
 import io from 'socket.io-client'
 const SocketContext = createContext()
 const SocketProvider = ({ children }) => {
-    const ENDPOINT = 'https://fortlax.herokuapp.com/';
+    const ENDPOINT = 'https://fortlax.herokuapp.com/';  //proxy this endpoint
     const socket = io(ENDPOINT, { transports: ['websocket', 'polling'] })
     return (
         <SocketContext.Provider value={socket}>
