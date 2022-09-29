0ff8340504196016870f667ae5f7481b33734831
Sambhav Saxena <sambhavsaxena02@gmail.com>
2022-09-29 16:13:36 +0530

    removed useless const regex

--- server/users.js
+++ server/users.js
@@ -1,13 +1,12 @@
 const users = []
-const regex = /^[^a-zA-Z0-9]+$/;
 const addUser = (id, name, room) => {
     const existingUser = users.find(user => user.name.trim().toLowerCase() === name.trim().toLowerCase())
     if (existingUser) return { error: "Username has already been taken" }
     if (!name && !room) return { error: "Username and room are required" }
     if (name.length < 3) return { error: "Username must be at least three characters" }
     if (room.length < 3) return { error: "Room name must be at least three characters" }
-    if (!name && !regex.test(name)) return { error: "Username must be alphanumeric" }
-    if (!room && !regex.test(room)) return { error: "Room name must be alphanumeric" }
+    if (!name) return { error: "Username is required" }
+    if (!room) return { error: "Room is required" }
     const user = { id, name, room }
     users.push(user)
     return { user }
